4c319da225a68396b60172b92f66ce3cef63f346
Devansh Bhikajee <devanshb39@gmail.com>
2015-02-10 20:50:08 +0000

    q

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+require config.assets.initialize_on_precompile = false
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -15,6 +16,6 @@ module Pinteresting
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.    # config.time_zone = 'Central Time (US & Canada)'
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
- config.assets.initialize_on_precompile = false
+ 
   end
 end
